f864445b1e68c38e285d6c84c1a8b6388d059a05
A20ura <andrea.ventura23993@gmail.com>
2023-11-15 18:12:40 +0100

    andrea ventura artist.js

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,3 +1,92 @@
 const urlParams = new URLSearchParams(window.location.search);
-const artistId = urlParams.get('artistId');
-console.log(artistId);
+const artistId = urlParams.get('id');
+const token = localStorage.getItem('token');
+
+if (artistId) {
+    const urlArtist = `https://api.spotify.com/v1/artists/${artistId}`;
+    const urlTracks = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=IT`;
+
+    fetchArtists(urlArtist, token);
+    fetchTracks(urlTracks, token);
+}
+
+function fetchArtists(url, token) {
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+    })
+        .then(response => response.json())
+        .then(data => { populateArtist(data) })
+}
+
+function fetchTracks(url, token) {
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            populateTracks(data)
+        })
+}
+
+function populateTracks(data) {
+    const tracksWrapper = document.querySelector('#tracksWrapper');
+
+    for (let i = 0; i < 10; i++) {
+
+        const divRow = document.createElement('div');
+        divRow.classList.add('row', 'align-items-center', 'mb-3', 'px-1', 'py-2', 'track-row');
+
+        const divCol1 = document.createElement('div');
+        divCol1.classList.add('col-1', 'track-id');
+
+        const spanNumber = document.createElement('span');
+        spanNumber.classList.add('number-hover');
+        spanNumber.textContent = '1';
+
+        const iPlay = document.createElement('i');
+        iPlay.classList.add('fa-solid', 'fa-play', 'd-none', 'play-hover');
+
+        divCol1.appendChild(spanNumber);
+        divCol1.appendChild(iPlay);
+
+        const divCol2 = document.createElement('div');
+        divCol2.classList.add('col-1', 'px-0', 'me-2', 'me-xl-0', 'track-img');
+
+        const img = document.createElement('img');
+        img.classList.add('img-popolar-art');
+        img.src = data.tracks[i].images[1].url;
+
+        divCol2.appendChild(img);
+        divRow.appendChild(divCol1);
+        divRow.appendChild(divCol2);
+
+        tracksWrapper.appendChild(divRow);
+
+    }
+
+}
+
+function populateArtist(data) {
+
+    const artistNameTitle = document.getElementById('artist-name');
+    const artistFollowersTitle = document.getElementById('visulizzazioni-art');
+    const artistBgImg = document.querySelector('.artist')
+
+    artistNameTitle.textContent = data.name;
+    artistFollowersTitle.textContent = data.followers.total;
+    artistBgImg.style.backgroundImage = `url(${data.images[0].url})`;
+    artistBgImg.style.backgroundSize = 'cover';
+    artistBgImg.style.backgroundPosition = 'center';
+    artistBgImg.style.backgroundRepeat = 'no-repeat';
+
+
+}
---
+++ assets/js/search.js
@@ -0,0 +1,155 @@
+const token = "BQACAfd1KcbTDa9ipPXO-w0PpeTCsMZoWfG02ojqHrMzfD7mQrNO627trQ5rDdtRp6G9vH-80mkNsvo2Z7NpCDTczYgoqhek7rkecXQQPloxNavHq5g"
+const inputField = document.getElementById('search-inp');
+
+inputField.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+
+        const querySearch = document.querySelector('#search-inp').value;
+        const urlSearch = `https://api.spotify.com/v1/search?q=${querySearch}&type=track%2Calbum%2Cartist%2Cplaylist `;
+
+        fetch(urlSearch, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+
+                populateSearchAlbums(data);
+                populateSearchArtist(data);
+                populateSearchTracks(data);
+            })
+            .catch(err => console.log(err));
+    }
+});
+
+function populateSearchAlbums(data) {
+    for (let i = 0; i <= 3; i++) {
+        const rowSearch = document.getElementById('search-row');
+
+        const divCol = document.createElement('div');
+        divCol.classList.add('col-6', 'col-lg-3', 'mb-4');
+
+        const anchorAlbum = document.createElement('a');
+        anchorAlbum.href = "album.html?id=" + data.tracks.items[i].album.id;
+
+        const divItemSearch = document.createElement('div');
+        divItemSearch.classList.add('items-search', 'rounded', 'pt-3', 'ps-1');
+        divItemSearch.style.backgroundImage = `url(${data.tracks.items[i].album.images[1].url})`
+        divItemSearch.style.backgroundSize = 'cover';
+        divItemSearch.style.backgroundPosition = 'center';
+
+        const h4 = document.createElement('h4');
+        h4.textContent = data.tracks.items[i].name;
+
+        divItemSearch.appendChild(h4);
+        anchorAlbum.appendChild(divItemSearch);
+        divCol.appendChild(anchorAlbum);
+        rowSearch.appendChild(divCol);
+    }
+}
+
+function fetchArtistsImg(url) {
+
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            artistImg = data.images[0].url
+        })
+        .catch(err => console.log(err));
+}
+
+function populateSearchArtist(data) {
+    for (let i = 0; i <= 3; i++) {
+        const rowSearch = document.getElementById('search-row');
+
+        const divCol = document.createElement('div');
+        divCol.classList.add('col-6', 'col-lg-3', 'mb-4');
+
+        const anchorArtist = document.createElement('a');
+        anchorArtist.href = "artists.html?id=" + data.tracks.items[i].artists[0].id;
+
+        const divItemSearch = document.createElement('div');
+        const artistId = data.tracks.items[i].artists[0].id;
+        const url = `https://api.spotify.com/v1/artists/${artistId}`
+        
+        let artistImg = fetchArtistsImg(url)
+
+        divItemSearch.classList.add('items-search', 'rounded', 'pt-3', 'ps-1');
+        divItemSearch.style.backgroundImage = `url(${artistImg})`
+        divItemSearch.style.backgroundSize = 'cover';
+        divItemSearch.style.backgroundPosition = 'center';
+
+        const h4 = document.createElement('h4');
+        h4.textContent = data.tracks.items[i].album.artists[0].name;
+
+        divItemSearch.appendChild(h4);
+        anchorArtist.appendChild(divItemSearch);
+        divCol.appendChild(anchorArtist);
+        rowSearch.appendChild(divCol);
+    }
+}
+
+function populateSearchTracks(data) {
+    for (let i = 0; i <= 3; i++) {
+        const rowSearch = document.getElementById('search-row');
+
+        const divCol = document.createElement('div');
+        divCol.classList.add('col-6', 'col-lg-3', 'mb-4');
+
+        const divItemSearch = document.createElement('div');
+        const trackId = data.tracks.items[i].id;
+        
+
+        divItemSearch.classList.add('items-search', 'rounded', 'pt-3', 'ps-1');
+        divItemSearch.style.backgroundImage = `url(${data.tracks.items[i].album.images[1].url})`
+        divItemSearch.style.backgroundSize = 'cover';
+        divItemSearch.style.backgroundPosition = 'center';
+
+        const h4 = document.createElement('h4');
+        h4.textContent = data.tracks.items[i].name;
+
+        divItemSearch.appendChild(h4);
+        divCol.appendChild(divItemSearch);
+        rowSearch.appendChild(divCol);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setBorderFocus() {
+    const inputSearch = document.querySelector('#search-inp')
+    const divToCustom = document.querySelector('.search-input-pg')
+    const iconSearch = document.querySelector('#search-icon')
+    inputSearch.addEventListener('focus', () => {
+        divToCustom.classList.add('search-border')
+        iconSearch.classList.add('text-white')
+    })
+    inputSearch.addEventListener('blur', () => {
+        divToCustom.classList.remove('search-border')
+        iconSearch.classList.remove('text-white')
+    })
+}
+
+setBorderFocus()
+
